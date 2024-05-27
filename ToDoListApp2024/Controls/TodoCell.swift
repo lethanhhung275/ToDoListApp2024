2b0d4cadeee9a390395d898418471843ba1ffdda
lethanhhung275 <lethanhhung275@gmail.com>
2024-05-27 13:58:00 +0700

    v1

---
+++ ToDoListApp2024/Controls/TodoCell.swift
@@ -0,0 +1,23 @@
+//
+//  TodoCell.swift
+//  ToDoListApp2024
+//
+//  Created by Lê Hưng on 26/5/24.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ToDoListApp2024/FWDB/DAL/Database.swift
@@ -0,0 +1,8 @@
+//
+//  Database.swift
+//  ToDoListApp2024
+//
+//  Created by Lê Hưng on 26/5/24.
+//
+
+import Foundation
